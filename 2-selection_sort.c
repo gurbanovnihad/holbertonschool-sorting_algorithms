d80044a2518971c3d8cec2b9cee7e1bd4f2f7de1
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-01-19 22:44:13 -0800

    betty style

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -28,7 +28,7 @@ void selection_sort(int *array, size_t size)
 			temp = array[i];
 			array[i] = array[min_index];
 			array[min_index] = temp;
-			print_array(array,size);
+			print_array(array, size);
 		}
 	}
 }
